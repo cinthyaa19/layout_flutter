69cac064ff53a0c6739c26fd41afb5f2300bb95e
Cinthya Achwatul Ifnu <cinthyaachwatulifnu@gmail.com>
2024-10-16 11:59:37 +0700

    Praktikum 1

--- README.md
+++ README.md
@@ -7,11 +7,48 @@ A new Flutter project.
     Kelas: TI 3H
     No: 07
 
-## Praktikum 1: Membangun Layout di Flutter
+## **Praktikum 1: Membangun Layout di Flutter**
 ### Langkah 2: File lib/main.dart
 ![alt](/images/01.png)
 
 ### Langkah 4: Implementasi title row
 ![alt](/images/02.png)
+<br><br>
 
-## Praktikum 2: Implementasi button row
+## **Praktikum 2: Implementasi button row**
+### Langkah 1: Buat method Column _buildButtonColumn
+![alt](/images/03.png)
+
+### Langkah 2: Buat widget buttonSection
+![alt](/images/04.png)
+
+### Langkah 3: Tambah button section ke body
+![alt](/images/05.png)
+
+### Hasil Output pada Browser
+![alt](/images/06.png)
+<br><br>
+
+## **Praktikum 3: Implementasi text section**
+### Langkah 1: Buat widget textSection
+![alt](/images/07.png)
+
+### Langkah 2: Tambahkan variabel text section ke body
+![alt](/images/08.png)
+
+### Hasil Output pada Browser
+![alt](/images/09.png)
+<br><br>
+
+## **Praktikum 4: Implementasi image section**
+### Langkah 1: Siapkan aset gambar
+![alt](/images/10.png)
+
+### Langkah 2: Tambahkan gambar ke body
+![alt](/images/11.png)
+
+### Langkah 3: Terakhir, ubah menjadi ListView
+![alt](/images/12.png)
+
+### Hasil Output pada Browser
+![alt](/images/13.png)
--- lib/main.dart
+++ lib/main.dart
@@ -7,39 +7,27 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter layout: Cinthya Achwatul Ifnu - 2241720051',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Cinthya Achwatul Ifnu - 2241720051'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
-      ),
-    );
-  }
     Widget titleSection = Container(
       padding: const EdgeInsets.all(32),
       child: Row(
         children: [
           Expanded(
-            /* soal 1*/
+            /*1*/
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                /* soal 2*/
+                /*2*/
                 Container(
                   padding: const EdgeInsets.only(bottom: 8),
                   child: const Text(
-                    'Wisata Gunung di Batu',
+                    'Wisata Pantai Pink',
                     style: TextStyle(
                       fontWeight: FontWeight.bold,
                     ),
                   ),
                 ),
                 const Text(
-                  'Batu, Malang, Indonesia',
+                  'Pulau Komodo, Nusa Tenggara Timur, Indonesia',
                   style: TextStyle(
                     color: Colors.grey,
                   ),
@@ -47,36 +35,80 @@ class MyApp extends StatelessWidget {
               ],
             ),
           ),
-          /* soal 3*/
-          const Icon(
+          /*3*/
+          Icon(
             Icons.star,
-            color: Colors.red,
+            color: Colors.red[500],
           ),
           const Text('41'),
         ],
       ),
     );
 
-    
+    Color color = Theme.of(context).primaryColor;
 
-//   Column _buildButtonColumn(Color color, IconData icon, String label) {
-//     return Column(
-//       mainAxisSize: MainAxisSize.min,
-//       mainAxisAlignment: MainAxisAlignment.center,
-//       children: [
-//         Icon(icon, color: color),
-//         Container(
-//           margin: const EdgeInsets.only(top: 8),
-//           child: Text(
-//             label,
-//             style: TextStyle(
-//               fontSize: 12,
-//               fontWeight: FontWeight.w400,
-//               color: color,
-//             ),
-//           ),
-//         ),
-//       ],
-//     );
-//   }
-// }
+    Widget buttonSection = Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        _buildButtonColumn(color, Icons.call, 'CALL'),
+        _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+        _buildButtonColumn(color, Icons.share, 'SHARE'),
+      ],
+    );
+
+    Widget textSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: const Text(
+        'Pantai Pink adalah salah satu tempat wisata yang unik dan menarik di Indonesia. '
+        'Terletak di Pulau Komodo, Nusa Tenggara Timur, pantai ini terkenal karena memiliki pasir berwarna pink yang langka dan indah. '
+        'Warna pink pada pasir pantai ini disebabkan oleh campuran pasir putih dan serpih merah muda yang berasal dari organisme laut '
+        'bernama foraminifera. Fenomena ini membuat Pantai Pink menjadi salah satu pantai berpasir pink yang paling terkenal di dunia.'
+        '\n\nCinthya Achwatul Ifnu - 2241720051',
+        softWrap: true,
+      ),
+    );
+         
+    return MaterialApp(
+      title: 'Flutter layout: Cinthya Achwatul Ifnu - 2241720051',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter layout demo'),
+        ),
+        body: ListView(
+          children: [
+            Image.asset(
+              'images/pantaipink.jpg',
+              width: 600,
+              height: 240,
+              fit: BoxFit.cover,
+            ),
+            titleSection,
+            buttonSection,
+            textSection,
+          ],
+        )
+      )
+    );
+  }
+
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
